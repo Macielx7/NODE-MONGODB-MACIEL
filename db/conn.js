4d91936c3bf0b8aaa687cd2a5761aba9fd392069
JoaoVictorRS <joaososteam@gmail.com>
2024-03-27 21:21:35 -0300

    controller criada

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 async function main() {
     try {
         await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusteriesb.ktr0p8y.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIESB`);
