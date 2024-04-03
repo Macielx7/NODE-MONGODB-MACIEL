acf2294e2af21de6500ccd9b71cf6c8a3f4db1e1
Lucas Fernandes da silva <lucas_fernadesss@hotmail.com>
2024-04-03 19:37:12 -0300

    NODEMON-DB:COMPLETO

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function main() {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clusteriesb.ktr0p8y.mongodb.net/?retryWrites=true&w=majority&appName=ClusterIESB`);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5zr3qmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("Banco conectado")
     } catch (error) {
         console.log('Erro: ' + error)
