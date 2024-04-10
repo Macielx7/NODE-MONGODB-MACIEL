bbe4e988a3d0e88d5608009120d154f9dd3f9506
Macielx7 <davimacielbsb@gmail.com>
2024-04-10 15:32:58 -0300

    atividade 06

--- db/conn.js
+++ db/conn.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function main() {
     try {
-        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5zr3qmk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d1cm7ez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log("Banco conectado")
     } catch (error) {
         console.log('Erro: ' + error)
@@ -10,3 +10,5 @@ async function main() {
 }
 
 module.exports = main
+
+//${process.env.DB_USER}:${process.env.DB_PASSWORD}
--- models/Cliente.js
+++ models/Cliente.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose')
 
 const schema =  mongoose.Schema({
-    nome: String
+    nome: {
+        type: String,
+        required: true,
+        maxLength: 10,
+        uppercase:true,
+        trin: true
+    },
+    Cpf:{type:Number,
+        required:true,
+        match:[/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, "CPF INVALIDO"],
+    },
+    telefone: {
+        type: String,
+        required: true,
+        match: [/^\(\d{2}\) 9\d{1} \d{4}-\d{4}$/, "TELEFONE INVÁLIDO. O formato correto é (XX) 9XXXX-XXXX"]
+    }
 })
 
 const Cliente = mongoose.model('Cliente', schema)
